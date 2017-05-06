ef65a7bd8cf624529eecd30b864d2c01b76dcd2e
Peter Woo <peterw@eff.org>
2017-05-05 17:41:41 -0700

    Only submit to ecfs api in production

--- app/workers/ecfs_worker.rb
+++ app/workers/ecfs_worker.rb
@@ -3,6 +3,8 @@ class EcfsWorker
     api_key = ENV.fetch("ECFS_API_KEY")
     ecfs_uri = "https://publicapi.fcc.gov/ecfs/filings?api_key=#{api_key}"
 
-    HTTParty.post(ecfs_uri, headers: { "Content-Type" => "application/json" }, body: payload.to_json)
+    if Padrino.env == :production
+      HTTParty.post(ecfs_uri, headers: { "Content-Type" => "application/json" }, body: payload.to_json)
+    end
   end
 end
