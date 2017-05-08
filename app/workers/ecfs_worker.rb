735262bb5018b853296a32181838b5844022a2f4
Peter Woo <peterw@eff.org>
2017-05-08 08:27:56 -0700

    Make sure jobs raise exception if the comment wasn't submitted

--- app/workers/ecfs_worker.rb
+++ app/workers/ecfs_worker.rb
@@ -1,10 +1,13 @@
 class EcfsWorker
+  class Error < Exception; end
+
   def self.submit_comment(payload)
     api_key = ENV.fetch("ECFS_API_KEY")
     ecfs_uri = "https://publicapi.fcc.gov/ecfs/filings?api_key=#{api_key}"
 
     if Padrino.env == :production
-      HTTParty.post(ecfs_uri, headers: { "Content-Type" => "application/json" }, body: payload.to_json)
+      response = HTTParty.post(ecfs_uri, headers: { "Content-Type" => "application/json" }, body: payload.to_json)
+      raise Error.new("HTTP #{response.code}") unless (200...300).include?(response.code)
     end
   end
 end
