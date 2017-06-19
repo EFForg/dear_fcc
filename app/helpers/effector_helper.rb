7ba83c52de8fde7a7690e5ad009e27078df32ffa
Peter Woo <peterw@eff.org>
2017-06-19 12:55:56 -0700

    Don't require zip code for effector (international filers)

--- app/helpers/effector_helper.rb
+++ app/helpers/effector_helper.rb
@@ -2,7 +2,7 @@ module DearFcc
   class App
     module EffectorHelper
       def sign_up_for_effector(filer)
-        NewsletterWorker.delay(queue: "signups").signup(filer.fetch("email"), filer.fetch("zip_code"))
+        NewsletterWorker.delay(queue: "signups").signup(filer.fetch("email"), filer["zip_code"])
       end
     end
 
