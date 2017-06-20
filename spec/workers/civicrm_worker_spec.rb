c307a2a693b05793c713050af7483290fb30d6bc
Peter Woo <peterw@eff.org>
2017-06-20 10:21:50 -0700

    Fix newsletter subscriptions

--- spec/workers/civicrm_worker_spec.rb
+++ spec/workers/civicrm_worker_spec.rb
@@ -4,7 +4,7 @@ describe CivicrmWorker do
   describe '.subscribe(email, zip_code)' do
     it "should call CivicrmClient#import_contact" do
       email, zip_code = "user@example.com", "94109"
-      expect_any_instance_of(CivicrmClient).to receive(:import_contact).with(email: email, zip_code: zip_code)
+      expect_any_instance_of(CivicrmClient).to receive(:import_contact).with(email: email, zip_code: zip_code, subscribe: true)
       CivicrmWorker.subscribe(email, zip_code)
     end
   end
