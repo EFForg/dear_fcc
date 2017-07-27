a55fd179959c3d176f06f2c70d9a2e2834c4e82a
Peter Woo <peterw@eff.org>
2017-07-27 10:31:59 -0700

    Only cache in production

--- app/app.rb
+++ app/app.rb
@@ -6,7 +6,7 @@ module DearFcc
     register Padrino::Cache
     enable :caching
 
-    if ENV.key?("MEMCACHE_HOST") && Padrino.env != :test
+    if ENV.key?("MEMCACHE_HOST") && Padrino.env == :production
       set :cache, Padrino::Cache.new(:Memcached,
                                      server: ENV.fetch("MEMCACHE_HOST"),
                                      exception_retry_limit: 1)
